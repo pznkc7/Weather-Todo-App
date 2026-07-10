c315dbdea4b0922408dcfed300a19d01f1ed773c
Puzan Kc <khatripujan35@gmail.com>
2026-07-10 18:48:45 +0545

    basic homepage template setup

--- weathersite/views.py
+++ weathersite/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'weather/index.html')
+    return render(request, 'weathersite/index.html')
+
+def home(request):
+    return render(request, 'weathersite/home.html')
